80bd89575f1a7520423daf584acfb63e4674af49
Mario <mario.saliba98@gmail.com>
2020-04-02 18:46:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { OauthSender } from 'react-oauth-flow';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    (async function getWebAheadUsers() {
+      try {
+        const { data } = await axios.get(
+          `https://api.github.com/orgs/webahead/members?access_token=${process.env.REACT_APP_GITHUB_API}`
+        );
+
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  if (!isLoggedIn) {
+    return (
+      <OauthSender
+        authorizeUrl="https://www.dropbox.com/oauth2/authorize"
+        clientId={process.env.CLIENT_ID}
+        redirectUri="https://www.yourapp.com/auth/dropbox"
+        state={{ from: '/settings' }}
+        render={({ url }) => <a href={url}>Connect to Dropbox</a>}
+      />
+    );
+  }
+
+  return <div className="App"></div>;
 }
 
 export default App;
